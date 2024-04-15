8172d60c65c260f12eb016a518c45006afa2f2b7
achandap <82042282+achandap@users.noreply.github.com>
2024-04-15 14:40:40 -0500

    Update block_public_access.py

--- block_public_access.py
+++ block_public_access.py
@@ -1,25 +1,28 @@
 import json
+import logging
 import boto3
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 def lambda_handler(event, context):
+    logger.debug('## EVENT')
+    logger.debug(event)
     # Extract the S3 bucket name from the event
     bucket_name = event['detail']['findings'][0]['Resources'][0]['Details']['AwsS3Bucket']['Name']
 
-    # Create an S3 resource
-    s3 = boto3.resource('s3')
+    # Create an S3 client
+    s3 = boto3.client('s3')
 
-    # Get the bucket object
-    bucket = s3.Bucket(bucket_name)
-
-    # Get the current public access block configuration
-    public_access_block = bucket.PublicAccessBlockConfiguration()
-
-    # Update the public access block configuration
-    public_access_block.update(
-        BlockPublicAcls=True,
-        IgnorePublicAcls=True,
-        BlockPublicPolicy=True,
-        RestrictPublicBuckets=True
+    # Block public access configuration for the bucket
+    s3.put_public_access_block(
+        Bucket=bucket_name,
+        PublicAccessBlockConfiguration={
+            'BlockPublicAcls': True,
+            'IgnorePublicAcls': True,
+            'BlockPublicPolicy': True,
+            'RestrictPublicBuckets': True
+        }
     )
 
     print(f'Public access blocked for bucket: {bucket_name}')
